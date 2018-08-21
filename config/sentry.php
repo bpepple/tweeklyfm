c597c637f74996909f13f69c60743126c949c17e
Scott Robinson <scott@dor.ky>
2018-08-21 20:42:14 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/sentry.php
+++ config/sentry.php
@@ -1,6 +1,16 @@
 <?php
 
-return array(
+/*
+ * This file is part of tweeklyfm/tweeklyfm
+ *
+ *  (c) Scott Wilcox <scott@dor.ky>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ */
+
+return [
     'dsn' => env('SENTRY_LARAVEL_DSN'),
 
     // capture release as git sha
@@ -11,4 +21,4 @@ return array(
 
     // Capture default user context
     'user_context' => true,
-);
+];
